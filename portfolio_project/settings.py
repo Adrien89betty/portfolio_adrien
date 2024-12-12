5c35cb1921097d60179ab78eadf903a2741bf9d4
adrien leveille <adrienleveille.contact@gmail.com>
2024-12-12 14:39:53 +0100

    fix => adding apps to settings.py.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
+    'about',
+    'cv',
+    'projects',
+    'contact',
 ]
 
 MIDDLEWARE = [
