9a3c93a73fef8320e60c9a4684ce667866f0d61f
adrien leveille <adrienleveille.contact@gmail.com>
2024-12-12 15:35:43 +0100

    creating models for cv app.

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Certification(models.Model):
+    title = models.CharField(max_length=200)
+    institution = models.CharField(max_length=200)
+    date_obtained = models.DateField()
+    description = models.TextField(blank=True, null=True)
+    logo = models.ImageField(upload_to='certification_logos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class CV(models.Model):
+    file = models.FileField(upload_to='cv_files/')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"CV updated at {self.updated_at}"
