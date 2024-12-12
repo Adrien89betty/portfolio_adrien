e155036138bbb394280a472a3d846da6d048b40c
adrien leveille <adrienleveille.contact@gmail.com>
2024-12-12 15:29:31 +0100

    Creating Project model in projects app.

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technologies = models.JSONField()
+    github_link = models.URLField()
+    demo_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
