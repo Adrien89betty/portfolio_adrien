83dd4c3965703b7d8e8a5decba0db7335c3532bb
adrien leveille <adrienleveille.contact@gmail.com>
2024-12-16 18:38:04 +0100

    fix: tests added to contact/test_views.py

--- contact/tests/test_views.py
+++ contact/tests/test_views.py
@@ -1,1 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse
 
+class AboutViewTest(TestCase):
+    def test_about_view_status_code(self):
+        response = self.client.get(reverse('contact:contact_form'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_view_template_used(self):
+        response = self.client.get(reverse('contact:contact_form'))
+        self.assertTemplateUsed(response, 'contact/contact.html')
