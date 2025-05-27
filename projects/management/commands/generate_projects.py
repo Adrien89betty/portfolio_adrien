47ab5f6d02794115de17b74a6d2464580a45e671
adrien leveille <adrienleveille.contact@gmail.com>
2025-05-27 16:59:08 +0200

    Added new project

--- projects/management/commands/generate_projects.py
+++ projects/management/commands/generate_projects.py
@@ -16,15 +16,11 @@ class Command(BaseCommand):
                 title="Ce Portfolio",
                 description="""Ce portfolio est un site web personnel conçu pour présenter mon profil de développeur back-end, mes compétences techniques, mes projets réalisés ainsi qu’un moyen de me contacter et il est entièrement responsive.
 
+                Sur le plan de l’architecture, le site repose sur une séparation entre le back-end et le front-end. Le back-end est développé avec Django et expose les données via des API REST construites avec Django REST Framework.
 
-Sur le plan de l’architecture, le site repose sur une séparation entre le back-end et le front-end. Le back-end est développé avec Django et expose les données via des API REST construites avec Django REST Framework.
+                Les données sont stockées dans une base de données PostgreSQL, robuste et adaptée à la production et sont ensuite récupérées dynamiquement par le front-end, développé avec Vue.js (intégré via CDN), qui s’occupe du rendu côté client.
 
-
-Les données sont stockées dans une base de données PostgreSQL, robuste et adaptée à la production et sont ensuite récupérées dynamiquement par le front-end, développé avec Vue.js (intégré via CDN), qui s’occupe du rendu côté client.
-
-
-Cette approche permet une grande flexibilité, une meilleure scalabilité et une séparation claire des responsabilités dans le code.
-""",
+                Cette approche permet une grande flexibilité, une meilleure scalabilité et une séparation claire des responsabilités dans le code.""",
                 technologies=["Python", "Django", "Django REST Framework", "JavaScript", "Vue.js", "HTML5", "CSS3", "PostgreSQL", "Git", "Heroku"],
                 github_link="https://github.com/Adrien89betty/portfolio_adrien",
                 demo_link="https://www.adrienleveille.com/",
@@ -33,11 +29,17 @@ Cette approche permet une grande flexibilité, une meilleure scalabilité et une
         self.stdout.write(self.style.SUCCESS(f"project « {project1.title} » recorded in database."))
 
         project2 = Project.objects.create(
-                title="Automatic Scraper",
-                description="Le projet sera visible prochainement",
-                technologies=["Python", "Scrapy", "Playwright", "JSON", "Xpath"],
-                github_link=fake.url(),
-                demo_link=fake.url(),
+                title="Smart Scraper",
+                description="""Smart Scraper est un projet de scraping web modulaire développé avec Scrapy et Scrapy-Playwright.
+
+                Conçu pour extraire les données produits des sites e-commerce, il est configurable via des fichiers JSON validés avec Pydantic.
+
+                Il se compose d'un spider principal qui extrait les données détaillées des produits et d'un spider auxiliaire qui extrait les URLs de détails d produit depuis une page de collection de produits.
+
+                De plus, un script utilitaire est fourni pour convertir les objets JSON de la sortie du spider auxiliaire en une liste Python d'URLs adaptées aux fichiers de configuration JSON.""",
+                technologies=["Python", "Scrapy", "Playwright", "JSON", "Xpath", "JSON-LD"],
+                github_link="https://github.com/Adrien89betty/smart-scraping",
+                demo_link="https://www.adrienleveille.com/#projects-section",
                 image=IMG_FILES[2]
             )
         self.stdout.write(self.style.SUCCESS(f"project « {project2.title} » recorded in database."))
